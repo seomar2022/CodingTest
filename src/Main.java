58e0075dcb77a2e44a858c8c325842e5f6d32fca
seomar <seomar2022@gmail.com>
2023-04-13 23:29:49 +0900

    string19, 20

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,48 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
+                Scanner sc = new Scanner(System.in);
+                String input = sc.nextLine();
+                String[] croatianAlphabet = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+                int sum = 0;
+
+                TreeSet<Integer> finds = new TreeSet<>();
+                for(String letter : croatianAlphabet){
+
+
+                    if(input.indexOf(letter)>=0){
+                        for(int i = 0; i<input.length(); i++){
+                            if(input.indexOf(letter, i)>=0){
+                                finds.add(input.indexOf(letter, i));
+                                if(letter.equals("z=") && i>0 && input.charAt(i-1) == 'd'){
+                                    finds.remove(i);
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+
+                sum = finds.size();
+
+
+                StringBuilder newString = new StringBuilder(input);
+
+
+                for(int find :finds){
+                    newString.setCharAt(find, '0');
+                    newString.setCharAt(find+1, '0');
+                }
+
+                input = newString.toString();
+                input = input.replace("=", "");
+
+                input = input.replaceAll("0", "");
+                sum += input.length();
+
+                System.out.println(sum);
+
+
     }
 }
--- src/string19.java
+++ src/string19.java
@@ -27,26 +27,39 @@ dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것�
 
 출력
 입력으로 주어진 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+
+ljes=njakはクロアチアアルファベット6つ(lj, e, ̌, nj, a, k)からなる。 単語が与えられた時、いくつのクロアチアアルファベットで構成されているかを出力する。
+
+ďは無条件に一つのアルファベットとして使われ、dと̌が分離されたとは考えない。 lj と nj も同様である。 上のリストにないアルファベットは一文字ずつ数える。
+
+入力
+1行目に最大100文字の単語が与えられる。 アルファベットの小文字と「-」、「=」だけで構成されている。
+
+単語はクロアチアのアルファベットで構成されている。 問題説明の表に出ているアルファベットは変更された形で入力される。
+
+出力
+入力として与えられた単語がいくつのクロアチアアルファベットで構成されているかを出力する。
          */
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String[] croatianAlphabet = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
         int sum = 0;
-        //ArrayList<Integer> finds = new ArrayList<>();
+
         TreeSet<Integer> finds = new TreeSet<>();
         for(String letter : croatianAlphabet){
-           // System.out.println("letter-> "+ letter);
+
 
             if(input.indexOf(letter)>=0){
                 for(int i = 0; i<input.length(); i++){
 
-                    if(input.indexOf(letter, i)>=0){
+                    if(input.indexOf(letter, i)>=0){//同じクロアチアアルファベットが重複される場合もあるため。
                         finds.add(input.indexOf(letter, i));
-                        if(letter.equals("z=") && i>0 && input.charAt(i-1) == 'd'){
+                        //if(letter.equals("z=") && i>0 && input.charAt(i-1) == 'd'){  <-このコードはddz=z=を入力する際に、間違う
+                        if(input.charAt(i) == 'z' && input.charAt(i+1) == '=' && i>0 && input.charAt(i-1) == 'd'){
                             finds.remove(i);
                         }
 
-                     //   System.out.println("chk -> "+input.indexOf(letter, i));
+                    //    System.out.println("chk -> "+input.indexOf(letter, i));
                     }
 
                 }
@@ -55,61 +68,39 @@ dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것�
             }
         }
 
+        sum = finds.size(); //クロアチアアルファベットの数を数える。
+
+        //.setCharAtを使うため、StringBuilder使う
+        StringBuilder inputToStringBuilder = new StringBuilder(input);
 
        // System.out.println(finds);
-        String copiedInput = input;
-        sum = finds.size();
-
-
-//        ArrayList<Character> input2 = new ArrayList<>(input.length());
-//        for(char a : input.toCharArray()){
-//            input2.add(a);
-//        }
-
-        StringBuilder newString = new StringBuilder(input);
-
-     //   System.out.println(finds);
         for(int find :finds){
-            //findからfind+1の位置にあるやつを削除、
-           // input = input.replace(copiedInput.substring(find, find+2), "0");
-            //input = input.replace(copiedInput.substring(find, find+2), "0");
+            //findからfind+1の位置にあるやつを0に入れ替える。削除したら、lljjなどを入力する時に間違える。
+            inputToStringBuilder.setCharAt(find, '0');
+            inputToStringBuilder.setCharAt(find+1, '0');
 
 
-            newString.setCharAt(find, '0');
-            newString.setCharAt(find+1, '0');
-
-           // System.out.println(newString);
-           // ordinaryAlphabet = copiedInput.substring()
-
-//            input2.remove(find);
-//            input2.remove(find+1);
-
-
-         //  System.out.println("find ->"+find);
-          // System.out.println("input1 ->" + input);
+       //    System.out.println("find ->"+find);
+         //  System.out.println("input1 ->" + input);
         }
+
         //dz=は3文字であるため、=が残っていたら削除
-        input = newString.toString();
+        input = inputToStringBuilder.toString();
         input = input.replace("=", "");
-       // input2.remove('=');
 
-     //   System.out.println(finds);
-      //  System.out.println(input);
-       // System.out.println("input2 ->" + input);
-        //残った物のlengthを数える
 
+        //System.out.println("input2 ->" + input);
+
+
+        //0を削除する
         input = input.replaceAll("0", "");
-        //System.out.println("input3 ->" + input);
+     //   System.out.println("input3 ->" + input);
+
+        //残った物のlengthを数えて、sumにたす。(一般のアルファベットを数える)
         sum += input.length();
 
-       // sum += input2.size();
 
         System.out.println(sum);
-
-
-
-
-
 
     }
 }
