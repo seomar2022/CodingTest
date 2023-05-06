9e95fb0e4ff5a4d09e6055aa524717a7f266a28a
seomar <seomar2022@gmail.com>
2023-05-06 19:22:51 +0900

    Math2

---
+++ src/math/Math2.java
@@ -0,0 +1,43 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Math2 {
+    public static void main(String[] args) {
+        /*
+        https://www.acmicpc.net/problem/11005
+        10진법 수 N이 주어진다. 이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
+
+        10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+
+        A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
+
+        입력
+        첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36) N은 10억보다 작거나 같은 자연수이다.
+
+        출력
+        첫째 줄에 10진법 수 N을 B진법으로 출력한다.
+         */
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int N = Integer.parseInt(input.split(" ")[0]);
+        int B = Integer.parseInt(input.split(" ")[1]);
+        ArrayList<Character> remainder = new ArrayList<>();
+
+      //  int dividend = 0;
+        while (N >= B){
+
+            System.out.println(N % B);
+            remainder.add((char)(N % B));
+           // System.out.println(N/B);
+            N = N/B;
+        }
+
+        System.out.println(remainder);
+
+
+
+    }
+}
