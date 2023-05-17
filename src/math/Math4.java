ca21d1bf406e254bf016f396672190e6f374e84b
seomar <seomar2022@gmail.com>
2023-05-17 23:23:27 +0900

    Math4

--- src/math/Math4.java
+++ src/math/Math4.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.Scanner;
+
 public class Math4 {
     public static void main(String[] args) {
         /*
@@ -26,27 +28,29 @@ public class Math4 {
 
         Mirko and Slavko are filming a movie adaptation of the popular SF novel "Chicks in space 13". The script requires them to present a lot of different worlds so they decided to film the entire movie in front of a green screen and add CGI backgrounds later. Mirko heard that the best way to generate artificial terrain is to use midpoint displacement algorithm.
 
-To start the algorithm, Mirko selects 4 points forming a perfect square. He then performs the following steps:
+        To start the algorithm, Mirko selects 4 points forming a perfect square. He then performs the following steps:
 
-On each side of the square, he adds a new point in the exact middle of the side. The height of this new point is the average height of the two points on that side.
-In the exact center of the square he adds a new point whose height is the average height of all 4 square vertices, plus a small random value.
-After those two steps are performed, he now has 4 new squares. He performs the same steps on the newly created squares again and again until he is pleased with the results. The following diagram illustrates 2 iterations of the algorithm.
-     Mirko noticed that some of the points belong to more than one square. In order to decrease memory consumption, he stores calculates and stores such points only once. He now wonders how many points in total will he need to store in memory after N iterations.
+        On each side of the square, he adds a new point in the exact middle of the side. The height of this new point is the average height of the two points on that side.
+        In the exact center of the square he adds a new point whose height is the average height of all 4 square vertices, plus a small random value.
+        After those two steps are performed, he now has 4 new squares. He performs the same steps on the newly created squares again and again until he is pleased with the results. The following diagram illustrates 2 iterations of the algorithm.
+             Mirko noticed that some of the points belong to more than one square. In order to decrease memory consumption, he stores calculates and stores such points only once. He now wonders how many points in total will he need to store in memory after N iterations.
 
-input
-The first and only line of input contains one integer N (1 ≤ N ≤ 15), number of iterations
+        input
+        The first and only line of input contains one integer N (1 ≤ N ≤ 15), number of iterations
 
-output
-The first and only line of output should contain one number, the number of points stored after N iterations.
+        output
+        The first and only line of output should contain one number, the number of points stored after N iterations.
 
          */
         // 0->4 2*2
         // 1->9 3*3
         // 2->25 5*5
         // 3->81 9*9
+        // n->(1+2^n)^2
 
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        // 1+2^n
-
+        System.out.println((int)Math.pow(1+Math.pow(2, N), 2));
     }
 }
