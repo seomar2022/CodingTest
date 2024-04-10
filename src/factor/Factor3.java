de4d01ff99a254ff905219d9cc9ce3260e768056
seomar <seomar2022@gmail.com>
2024-04-10 23:59:13 +0900

    Factor3

---
+++ src/factor/Factor3.java
@@ -0,0 +1,27 @@
+package factor;
+
+import java.util.Scanner;
+
+public class Factor3 {
+    public static void main(String[] args) {
+
+
+    /*
+    https://www.acmicpc.net/problem/9506
+    En route to Rigel 7, Chief Engineer Geordi Laforge and Data were discussing favorite numbers. Geordi exclaimed he preferred Narcissistic Numbers: those numbers whose value is the same as the sum of the digits of that number, where each digit is raised to the power of the number of digits in the number.
+
+Data agreed that Narcissistic Numbers were interesting, but not as good as his favorite: Perfect Numbers. Geordi had never heard of a Perfect Number, so Data elaborated, “A positive integer is said to be Perfect if it is equal to the sum of its positive divisors less than itself. For example, 6 is Perfect because 6 = 1 + 2 + 3.”
+
+Geordi began thinking about an algorithm to determine if a number was Perfect, but did not have the raw computing ability of Data. He needs a program to determine if a given number is Perfect.
+     */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+
+        int factor = 2;
+        while (factor < n) {
+
+        }
+
+    }
+}
