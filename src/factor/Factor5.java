1364c200b8749267f5f2b2cc3588e46ecf9555e2
seomar <seomar2022@gmail.com>
2024-04-18 22:43:26 +0900

    Factor5

---
+++ src/factor/Factor5.java
@@ -0,0 +1,20 @@
+package factor;
+
+import java.util.Scanner;
+
+public class Factor5 {
+    public static void main(String[] args) {
+        /*
+        https://www.acmicpc.net/problem/2581
+        자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
+
+        예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 이들 소수의 합은 620이고, 최솟값은 61이 된다.
+
+         */
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+    }
+
+}
